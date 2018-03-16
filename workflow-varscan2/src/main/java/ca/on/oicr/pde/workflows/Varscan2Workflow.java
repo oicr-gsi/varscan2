8f725d3cc44228ff0a4a164c1e20019c0e61dbd7
prisnir <Prisni.Rath@oicr.on.ca>
2018-03-16 13:34:45 -0400

    varscan2 single sample exec check

--- workflow-varscan2/src/main/java/ca/on/oicr/pde/workflows/Varscan2Workflow.java
+++ workflow-varscan2/src/main/java/ca/on/oicr/pde/workflows/Varscan2Workflow.java
@@ -47,8 +47,8 @@ public class Varscan2Workflow extends OicrWorkflow {
     private String samtools;
     private String java;
     private String varscan;
-    private String bcftools;
-    private String vcftools;
+//    private String bcftools;
+//    private String vcftools;
     private String tabix;
     private String bgzip;
 
@@ -91,6 +91,10 @@ public class Varscan2Workflow extends OicrWorkflow {
 
             //samtools
             samtools = getProperty("samtools");
+            
+            //tabix
+            tabix = getProperty("tabix");
+            bgzip = getProperty("bgzip");
             
             //varscan
             java = getProperty("java");
@@ -145,44 +149,57 @@ public class Varscan2Workflow extends OicrWorkflow {
         String tumourPileupFile = this.dataDir + this.outputFilenamePrefix + ".tumour.mpileup";
         Job tumourPileup = generateMpileup(inputTumourBam, tumourPileupFile);
         parentJob = tumourPileup;
-        if (this.normalBam != null) {
-            // general normal pileup
-            String inputNormalBam = getFiles().get("normal").getProvisionedPath();
-            String normalPileupFile = this.dataDir + this.outputFilenamePrefix + ".normal.mpileup";
-            Job normalPileup = generateMpileup(inputNormalBam, normalPileupFile);
-            normalPileup.addParent(parentJob);
-            parentJob = normalPileup;
-            
-            // SNP calls
-            Job somaticCall = runVarScanSomatic(tumourPileupFile, normalPileupFile);
-            somaticCall.addParent(parentJob);
-            
-            // Provision VCF
-            String provOutVCF = this.dataDir + this.outputFilenamePrefix + ".varscan.somatic.vcf";
-            SqwFile outVCF = createOutputFile(provOutVCF, VCF_METATYPE, this.manualOutput);
-            outVCF.getAnnotations().put("somatic VCF", "varscan");
-            somaticCall.addFile(outVCF);
-            
-            //copy number
-            Job copyNumber = runVarScanCopyNumber(tumourPileupFile, normalPileupFile);
-            copyNumber.addParent(parentJob);
-            
-            // Provision Copynumber file
-            String provCopyNumber = this.dataDir + this.outputFilenamePrefix + ".copynumber";
-            SqwFile outCopyNumber = createOutputFile(provCopyNumber, COPYNUMBER_METATYPE, this.manualOutput);
-            outCopyNumber.getAnnotations().put("copy number", "varscan");
-            copyNumber.addFile(outCopyNumber);
-        }
-        else {
-            Job germlineCall = runVarScanGermline(tumourPileupFile);
-            germlineCall.addParent(parentJob);
-            
-            // Provision VCF
-            String provOutVCF = this.dataDir + this.outputFilenamePrefix + ".varscan.germline.vcf";
-            SqwFile outVCF = createOutputFile(provOutVCF, VCF_METATYPE, this.manualOutput);
-            outVCF.getAnnotations().put("germline VCF", "varscan");
-            germlineCall.addFile(outVCF);
-        }
+//        if (this.normalBam != null) {
+//            // general normal pileup
+//            String inputNormalBam = getFiles().get("normal").getProvisionedPath();
+//            String normalPileupFile = this.dataDir + this.outputFilenamePrefix + ".normal.mpileup";
+//            Job normalPileup = generateMpileup(inputNormalBam, normalPileupFile);
+//            normalPileup.addParent(parentJob);
+//            parentJob = normalPileup;
+//            
+//            // SNP calls
+//            Job somaticCall = runVarScanSomatic(tumourPileupFile, normalPileupFile);
+//            somaticCall.addParent(parentJob);
+//            
+//            // Provision VCF
+//            String provOutVCF = this.dataDir + this.outputFilenamePrefix + ".varscan.somatic.vcf";
+//            SqwFile outVCF = createOutputFile(provOutVCF, VCF_METATYPE, this.manualOutput);
+//            outVCF.getAnnotations().put("somatic VCF", "varscan");
+//            somaticCall.addFile(outVCF);
+//            
+//            //copy number
+//            Job copyNumber = runVarScanCopyNumber(tumourPileupFile, normalPileupFile);
+//            copyNumber.addParent(parentJob);
+//            
+//            // Provision Copynumber file
+//            String provCopyNumber = this.dataDir + this.outputFilenamePrefix + ".copynumber";
+//            SqwFile outCopyNumber = createOutputFile(provCopyNumber, COPYNUMBER_METATYPE, this.manualOutput);
+//            outCopyNumber.getAnnotations().put("copy number", "varscan");
+//            copyNumber.addFile(outCopyNumber);
+//        }
+//        else {
+        Job germlineCall = runVarScanSingleSampleMode(tumourPileupFile);
+        germlineCall.addParent(parentJob);
+        parentJob = germlineCall;
+        String provOutVCF = this.dataDir + this.outputFilenamePrefix + ".varscan.germline.vcf";
+        
+        // bgzip and tabix index
+        Job tabixIndexVCF = tabixIndex(provOutVCF);
+        tabixIndexVCF.addParent(parentJob);
+        parentJob = tabixIndexVCF;
+        
+        
+        // Provision VCF
+        String gzProvOutVCF = provOutVCF + ".gz";
+        SqwFile outVCF = createOutputFile(gzProvOutVCF, VCF_METATYPE, this.manualOutput);
+        outVCF.getAnnotations().put("single sample VCF", "varscan");
+        parentJob.addFile(outVCF);
+        
+        String tbiOut = gzProvOutVCF + ".tbi"; 
+        SqwFile indexOut = createOutputFile(tbiOut, VCF_METATYPE, this.manualOutput);
+        indexOut.getAnnotations().put("index file for VCF", "varscan");
+        parentJob.addFile(indexOut);
+//        }
         
         
     }
@@ -201,7 +218,7 @@ public class Varscan2Workflow extends OicrWorkflow {
         return ssmpileup;
     }   
     
-    private Job runVarScanGermline(String tumourPileupFile) {
+    private Job runVarScanSingleSampleMode(String tumourPileupFile) {
         Job ssSNP = getWorkflow().createBashJob("varscan_germline");
         Command cmd = ssSNP.getCommand();
         cmd.addArgument(this.java);
@@ -219,48 +236,71 @@ public class Varscan2Workflow extends OicrWorkflow {
         return ssSNP;
     }
 
-    private Job runVarScanSomatic(String tumourPileupFile, String normalPileupFile){
-        Job somaticSNP = getWorkflow().createBashJob("varscan_Somatic");
-        Command cmd = somaticSNP.getCommand();
-        cmd.addArgument(this.java);
-        cmd.addArgument(this.javaMem);
-        cmd.addArgument("-jar "+this.varscan);
-        cmd.addArgument("somatic");
-        cmd.addArgument(tumourPileupFile);
-        cmd.addArgument(normalPileupFile);
-        cmd.addArgument("--output-vcf 1");
-        cmd.addArgument("--min-var-freq " + this.minVarFreq.toString());
-        cmd.addArgument("--min-coverage-tumour " + this.minCovTumour.toString());
-        cmd.addArgument("--min-coverage-normal " + this.minCovNormal.toString());
-        cmd.addArgument("> " + this.dataDir + this.outputFilenamePrefix+".varscan.somatic.vcf");
-        somaticSNP.setMaxMemory(Integer.toString(varscanMem * 1024));
-        somaticSNP.setQueue(this.queue);
-        return somaticSNP;
-    }
+//    private Job runVarScanSomatic(String tumourPileupFile, String normalPileupFile){
+//        Job somaticSNP = getWorkflow().createBashJob("varscan_Somatic");
+//        Command cmd = somaticSNP.getCommand();
+//        cmd.addArgument(this.java);
+//        cmd.addArgument(this.javaMem);
+//        cmd.addArgument("-jar "+this.varscan);
+//        cmd.addArgument("somatic");
+//        cmd.addArgument(tumourPileupFile);
+//        cmd.addArgument(normalPileupFile);
+//        cmd.addArgument("--output-vcf 1");
+//        cmd.addArgument("--min-var-freq " + this.minVarFreq.toString());
+//        cmd.addArgument("--min-coverage-tumour " + this.minCovTumour.toString());
+//        cmd.addArgument("--min-coverage-normal " + this.minCovNormal.toString());
+//        cmd.addArgument("> " + this.dataDir + this.outputFilenamePrefix+".varscan.somatic.vcf");
+//        somaticSNP.setMaxMemory(Integer.toString(varscanMem * 1024));
+//        somaticSNP.setQueue(this.queue);
+//        return somaticSNP;
+//    }
 
-    private Job runVarScanCopyNumber(String tumourPileupFile, String normalPileupFile) {
-        Job vsCNA = getWorkflow().createBashJob("varscan_CNA");
-        Command cmd = vsCNA.getCommand();
-//        cmd.addArgument("cd " + this.outDir + ";");
-        cmd.addArgument(this.java);
-        cmd.addArgument(this.javaMem);
-        cmd.addArgument("-jar " + this.varscan);
-        cmd.addArgument("copynumber");
-        cmd.addArgument(tumourPileupFile);
-        cmd.addArgument(normalPileupFile);
-        cmd.addArgument(this.dataDir + this.outputFilenamePrefix);
-        cmd.addArgument("--min-base-qual "+this.minBaseQual);
-        cmd.addArgument("--min-map-qual "+this.minMapQ);
-        cmd.addArgument("--min-coverage "+this.minCov);
-        vsCNA.setMaxMemory(Integer.toString(varscanMem * 1024));
-        vsCNA.setQueue(this.queue);
-        return vsCNA;
-    }
+//    private Job runVarScanCopyNumber(String tumourPileupFile, String normalPileupFile) {
+//        Job vsCNA = getWorkflow().createBashJob("varscan_CNA");
+//        Command cmd = vsCNA.getCommand();
+////        cmd.addArgument("cd " + this.outDir + ";");
+//        cmd.addArgument(this.java);
+//        cmd.addArgument(this.javaMem);
+//        cmd.addArgument("-jar " + this.varscan);
+//        cmd.addArgument("copynumber");
+//        cmd.addArgument(tumourPileupFile);
+//        cmd.addArgument(normalPileupFile);
+//        cmd.addArgument(this.dataDir + this.outputFilenamePrefix);
+//        cmd.addArgument("--min-base-qual "+this.minBaseQual);
+//        cmd.addArgument("--min-map-qual "+this.minMapQ);
+//        cmd.addArgument("--min-coverage "+this.minCov);
+//        vsCNA.setMaxMemory(Integer.toString(varscanMem * 1024));
+//        vsCNA.setQueue(this.queue);
+//        return vsCNA;
+//    }
     
     // tabix index
-//    private Job tabixIndex(String vcfFile){
-//        Job tabixIndexVCF = getWorkflow().createBashJob("tabix_index_vcf");
-//        Command cmd = tabixIndexVCF.getCommand();
-//        cmd.addArgument(this.bgzip)
+    private Job tabixIndex(String vcfFile){
+        Job tabixIndexVCF = getWorkflow().createBashJob("tabix_index_vcf");
+        Command cmd = tabixIndexVCF.getCommand();
+        cmd.addArgument(this.bgzip);
+        cmd.addArgument(vcfFile + ";");
+        cmd.addArgument(this.tabix + "-p vcf "+ vcfFile+".gz");
+        tabixIndexVCF.setMaxMemory(Integer.toString(varscanMem * 1024));
+        tabixIndexVCF.setQueue(this.queue);
+        return tabixIndexVCF;
+    }
+    
+//    private Job vcfPostProcessSingleSample(String inVCFgz){
+//        Job vcfPostProcess = getWorkflow().createBashJob("post_process_VCF");
+//        Command cmd = vcfPostProcess.getCommand();
+//        cmd.addArgument("export PATH=$PATH:"+this.vcftools+":"+this.bcftools);
+//        cmd.addArgument(this.vcftools+"vcf-merge " + inVCFgz + " " + inVCFgz +";");
+//        String tumorNameId = this.outputFilenamePrefix + ".tumor";
+//        String normalNameId = this.outputFilenamePrefix + ".blood";
+//        cmd.addArgument("cat "+ tumorNameId + " " + normalNameId + ">" + this.outputFilenamePrefix+".reheader.txt"+";");
+//        cmd.addArgument(this.bcftools+"/bcftools");
+//        cmd.addArgument("reheader");
+//        cmd.addArgument("--samples");
+//        cmd.addArgument(this.outputFilenamePrefix+".reheader.txt");
+//        cmd.addArgument(this.outputFilenamePrefix+".temp.vcf");
+//        vcfPostProcess.setMaxMemory(Integer.toString(varscanMem * 1024));
+//        vcfPostProcess.setQueue(this.queue);
+//        return vcfPostProcess;
 //    }
 }
